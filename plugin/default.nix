666f56dda9af13db8476f5209bf622bef85b94d6
Max Bolotin <maks.bolotin@gmail.com>
2025-06-06 23:06:05 +0200

    installPhase install at root level

--- plugin/default.nix
+++ plugin/default.nix
@@ -4,4 +4,9 @@ buildNpmPackage {
   version = "1.0.0";
   src = ./.;
   npmDepsHash = "sha256-JJV1EIT8LpFf4705clZLjHnDxZpA7m15QgQybGIasMc=";
+
+  installPhase = ''
+    mkdir -p $out
+    cp -R $src/* $out
+  '';
 }
